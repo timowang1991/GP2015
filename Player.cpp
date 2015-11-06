0ccb21ec6e4e16bba5448f37e96da474504e26eb
Wuteyan <killerw74@gmail.com>
2015-11-07 00:54:19 +0800

    player moving

--- Player.cpp
+++ Player.cpp
@@ -19,11 +19,10 @@ void Player::Init(ROOMid terrainRoomID)
 	actor.SetTerrainRoom(terrainRoomID, 10.0f); //Put the character on terrain
 
 	float pos[3], fDir[3], uDir[3]; //Set the character position and facing direction & put it on terrain
-	pos[0] = 3569.0f; pos[1] = -3208.0f; pos[2] = 1000.0f;
-	fDir[0] = 1.0f; fDir[1] = 1.0f; fDir[2] = 0.0f;
-	uDir[0] = 0.0f; uDir[1] = 0.0f; uDir[2] = 1.0f;
+	fDir[0] = 0.0f; fDir[1] = 1.0f; fDir[2] = 0.0f;
+	uDir[0] = 0.0f; uDir[1] = 0.0f; uDir[2] = 0.0f;
 	actor.SetDirection(fDir, uDir);
-	BOOL4 beOK = actor.PutOnTerrain(pos);
+	//BOOL4 beOK = actor.PutOnTerrain(pos);
 
 	//set animation
 	idleID = actor.GetBodyAction(NULL, "Idle");
@@ -32,7 +31,6 @@ void Player::Init(ROOMid terrainRoomID)
 	// set the character to idle action
 	curPoseID = FAILED_ID;
 	nextPosID = idleID;
-	actor.TurnRight(90.0f);
 }
 void Player::Update(int skip)
 {
@@ -42,7 +40,7 @@ void Player::Update(int skip)
 
 	if (speed != 0)
 	{
-		actor.MoveForward(5, FALSE, FALSE, 0, TRUE);
+		actor.MoveForward(speed, FALSE, FALSE, 0, TRUE);
 	}
 
 	BehaviorUpdate(skip);
@@ -74,7 +72,7 @@ void Player::BehaviorUpdate(int skip)
 
 	if (curPoseID == runID)
 	{
-		if (FyCheckHotKeyStatus(FY_UP) == FALSE)
+		if (!FyCheckHotKeyStatus(FY_UP) && !FyCheckHotKeyStatus(FY_RIGHT) && !FyCheckHotKeyStatus(FY_LEFT) && !FyCheckHotKeyStatus(FY_DOWN))
 		{
 			nextPosID = idleID;
 			speed = 0;
@@ -92,14 +90,36 @@ void Player::Input(BYTE code, BOOL4 value)
 	actor.ID(actorID);
 
 	if (code == FY_UP){
-		speed = 5;
-		nextPosID = runID;
+		if (value)
+		{
+			speed = 5;
+			nextPosID = runID;
+		}
+	}
+	if (code == FY_DOWN){
+		if (value)
+		{
+			speed = 5;
+			nextPosID = runID;
+			actor.TurnRight(180);
+		}
 	}
 	if (code == FY_RIGHT){
-		actor.TurnRight(5);
+		if (value)
+		{
+			speed = 5;
+			nextPosID = runID;
+			actor.TurnRight(10);
+		}
 	}
 	if (code == FY_LEFT){
-		actor.TurnRight(-5);
+		if (value)
+		{
+			speed = 5;
+			nextPosID = runID;
+			actor.TurnRight(-10);
+		}
 	}
-	
+	if (value == 0)
+		printf("%d",value);
 }
